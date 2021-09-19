2f52a4fb3f893c232bd3f46ec5d2331644a801c3
ShiCheng Lu <shichenglu0708@gmail.com>
2021-09-18 21:54:44 -0400

    reorganized folder structures

---
+++ build/CMakeFiles/mcTools.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mcTools.dir/main.cpp.obj"
+  "CMakeFiles/mcTools.dir/main.cpp.obj.d"
+  "libmcTools.dll.a"
+  "mcTools.exe"
+  "mcTools.exe.manifest"
+  "mcTools.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mcTools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mcTools.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mcTools.
+# This may be replaced when dependencies are built.
